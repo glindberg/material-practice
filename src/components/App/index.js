99bb425cc5b42be9d075a49a8402c1458cda3b4e
Gustav Lindberg <gustav.lindberg1@gmail.com>
2019-09-05 13:46:06 +0200

    Initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state= {
+            loading: true,
+        };
+    } 
+
+
+render() {
+return (
+    <h1>Hej jag gillar katter</h1>
+)
+}
+
+
+}
+
+export default App;
